5e3953fb07e9ccf5c5951f9d785dbc4cb380854f
caihai <ch15519060282@163.com>
2021-02-20 13:30:10 +0800

    完善面包屑功能

--- src/layout/Header/Breadcrumb.vue
+++ src/layout/Header/Breadcrumb.vue
@@ -1,35 +1,48 @@
 <template>
   <el-breadcrumb class="app-breadcrumb" separator="/">
     <transition-group name="breadcrumb">
-      <el-breadcrumb-item v-for="item in levelList" :key="item.path">
-        <router-link :to="{ path: item.path }">{{
-          item.meta.title
-        }}</router-link>
+      <el-breadcrumb-item
+        v-for="item in levelList"
+        :key="item.path || item.title"
+      >
+        <router-link v-if="item.path" :to="{ path: item.path }">
+          {{ item.title }}
+        </router-link>
+        <span v-else>{{ item.title }}</span>
       </el-breadcrumb-item>
     </transition-group>
   </el-breadcrumb>
 </template>
 
 <script>
+import { treeFilter } from "@/utils/util";
+
 export default {
-  data() {
-    return {
-      levelList: [],
-    };
-  },
-  mounted() {
-    this.getBreadcrumb();
-  },
-  methods: {
-    getBreadcrumb() {
-      this.levelList = this.$route.matched.filter(
-        (item) => item.meta && item.meta.title
+  computed: {
+    levelList() {
+      const { path } = this.$route;
+
+      const list = treeFilter(
+        this.$store.state.layout.menuList,
+        (item) => item.path && item.path === path
       );
+
+      return this.treeTOList(list);
     },
   },
-  watch: {
-    $route() {
-      this.getBreadcrumb();
+  methods: {
+    // 将一条枝干的树转成一维数组
+    treeTOList(treeList) {
+      const list = [];
+      while (treeList) {
+        let item = treeList[0];
+        list.push({
+          title: item.title,
+          path: item.path,
+        });
+        treeList = item.children;
+      }
+      return list;
     },
   },
 };
