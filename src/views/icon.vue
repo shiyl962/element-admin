b4a4db408ba692f774761a08176cd602e9b30de4
caihai <ch15519060282@163.com>
2021-02-23 16:13:31 +0800

    结构调整

--- src/views/icon.vue
+++ src/views/icon.vue
@@ -1,39 +1,37 @@
 <template>
-  <div>
-    <div class="head">
-      <el-tabs v-model="active" type="border-card">
-        <el-tab-pane label="iconfont" name="first">
-          <el-input
-            v-model="searchValue"
-            suffix-icon="el-icon-search"
-            placeholder="在次搜索图标，点击图标可复制"
-          ></el-input>
-        </el-tab-pane>
-        <el-tab-pane label="el-iocn" name="second">
-          <el-input
-            v-model="searchValue"
-            suffix-icon="el-icon-search"
-            placeholder="在次搜索图标，点击图标可复制"
-          ></el-input>
-          <div class="item-box">
-            <div
-              @click="handleClipboard(item, $event)"
-              class="icon-item"
-              v-for="item in elIcon.filter(
-                (item) =>
-                  !searchValue ||
-                  item.toLowerCase().includes(searchValue.toLowerCase())
-              )"
-              :key="item"
-            >
-              <i :class="item"></i>
-              <span>{{ item }}</span>
-            </div>
-          </div>
-        </el-tab-pane>
-      </el-tabs>
-    </div>
-  </div>
+  <el-tabs v-model="active" type="border-card">
+    <el-tab-pane label="iconfont" name="first">
+      <el-input
+        v-model="searchValue"
+        suffix-icon="el-icon-search"
+        placeholder="在次搜索图标，点击图标可复制"
+        style="width: 300px"
+      ></el-input>
+    </el-tab-pane>
+    <el-tab-pane label="el-iocn" name="second">
+      <el-input
+        v-model="searchValue"
+        suffix-icon="el-icon-search"
+        placeholder="在次搜索图标，点击图标可复制"
+        style="width: 300px"
+      ></el-input>
+      <div class="item-box">
+        <div
+          @click="handleClipboard(item, $event)"
+          class="icon-item"
+          v-for="item in elIcon.filter(
+            (item) =>
+              !searchValue ||
+              item.toLowerCase().includes(searchValue.toLowerCase())
+          )"
+          :key="item"
+        >
+          <i :class="item"></i>
+          <span>{{ item }}</span>
+        </div>
+      </div>
+    </el-tab-pane>
+  </el-tabs>
 </template>
 
 <script>
@@ -56,12 +54,6 @@ export default {
 </script>
 
 <style scoped>
-/* .container {
-} */
-
-.head .el-input {
-  width: 300px;
-}
 .item-box {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
