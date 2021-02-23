2d07e3373e1568445840636769a8e05750e9303f
caihai <ch15519060282@163.com>
2021-02-23 15:08:05 +0800

    完善table内修改

--- src/views/permis/Menu.vue
+++ src/views/permis/Menu.vue
@@ -1,26 +1,72 @@
 <template>
   <div>
     <el-table :data="filterTableData" border stripe style="width: 100%">
+      <div slot="append" class="tab-footer">
+        <el-button
+          @click="addMenuItem"
+          type="primary"
+          icon="el-icon-document-add"
+          >新 增</el-button
+        >
+      </div>
       <el-table-column type="index" label="#"> </el-table-column>
-      <el-table-column prop="title" label="菜单名称"> </el-table-column>
-      <el-table-column prop="path" label="路径"> </el-table-column>
-      <el-table-column prop="icon" label="图标"> </el-table-column>
+      <el-table-column label="菜单名称">
+        <TableEditItem
+          :ref="'rowTitle' + scope.$index"
+          slot-scope="scope"
+          :edit="scope.row.edit"
+          v-model="scope.row.title"
+          placeholder="菜单名"
+        ></TableEditItem>
+      </el-table-column>
+      <el-table-column label="路径">
+        <TableEditItem
+          :ref="'rowPath' + scope.$index"
+          slot-scope="scope"
+          :edit="scope.row.edit"
+          v-model="scope.row.path"
+          placeholder="路径"
+        ></TableEditItem>
+      </el-table-column>
+      <el-table-column label="图标">
+        <TableEditItem
+          :ref="'rowIcon' + scope.$index"
+          slot-scope="scope"
+          type="icon"
+          :edit="scope.row.edit"
+          v-model="scope.row.icon"
+          placeholder="图标"
+        ></TableEditItem>
+      </el-table-column>
 
       <el-table-column label="操作">
         <!-- eslint-disable-next-line vue/no-unused-vars -->
         <div class="box-flex" slot="header" slot-scope="scope">
-          <el-button type="primary" icon="el-icon-document-add"
-            >新 增</el-button
-          >
           <el-input
             v-model="searchValue"
             clearable
             placeholder="输入关键字搜索"
           />
         </div>
-        <template>
-          <el-button size="small" type="primary" plain>编辑</el-button>
-          <el-button type="danger" size="small">删除</el-button>
+        <template slot-scope="scope">
+          <template v-if="scope.row.edit">
+            <el-button @click="submit(scope)" size="small" type="primary">
+              保存
+            </el-button>
+            <el-button @click="editCancel(scope)" size="small">取消</el-button>
+          </template>
+          <el-button
+            v-else
+            @click="scope.row.edit = true"
+            size="small"
+            type="primary"
+            plain
+          >
+            编辑
+          </el-button>
+          <el-button v-if="!scope.row.isAdd" type="danger" size="small">
+            删除
+          </el-button>
         </template>
       </el-table-column>
     </el-table>
@@ -28,8 +74,10 @@
 </template>
 
 <script>
+import TableEditItem from "@/components/TableEditItem";
 export default {
   name: "Menu",
+  components: { TableEditItem },
   data() {
     return {
       searchValue: "",
@@ -52,18 +100,44 @@ export default {
       );
     },
   },
-  created() {},
-  mounted() {},
-  methods: {},
-  watch: {},
+  created() {
+    this.tableData.forEach((item) => {
+      this.$set(item, "edit", false); // 使 edit 能被vue检测
+    });
+  },
+  methods: {
+    // 新增
+    addMenuItem() {
+      this.tableData.push({
+        title: "",
+        path: "",
+        icon: "",
+        edit: true,
+        isAdd: true,
+      });
+    },
+    // 取消编辑
+    editCancel(scope) {
+      if (scope.row.isAdd) {
+        this.tableData.splice(scope.$index, 1);
+        return;
+      }
+      scope.row.edit = false;
+      scope.row.title = this.$refs[`rowTitle${scope.$index}`].initValue;
+      scope.row.path = this.$refs[`rowPath${scope.$index}`].initValue;
+      scope.row.icon = this.$refs[`rowIcon${scope.$index}`].initValue;
+    },
+    // 保存
+    submit(scope) {
+      console.log(scope);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.box-flex {
-  display: flex;
-}
-.box-flex > .el-button {
-  margin-right: 10px;
+.tab-footer {
+  padding: 20px;
+  text-align: right;
 }
 </style>
